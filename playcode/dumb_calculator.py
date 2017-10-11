6f68a188fd9fef11372137ab12fbdee9a8d24833
Renata Kilian <renatakiliandev@gmail.com>
2017-10-11 17:19:23 -0300

    Fix float :bug:

--- playcode/dumb_calculator.py
+++ playcode/dumb_calculator.py
@@ -24,9 +24,9 @@ num3 = input("Enter third number:")
 #}
 '''
 
-num1 = input("Enter first number:")
-num2 = input("Enter second number:")
-num3 = input("Enter third number:")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
 
 total = num1 + num2 + num3 / 2
 
