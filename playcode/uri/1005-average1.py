bd3c63eb005e065081113996c99cbd24504d6648
Renata Kilian <renatakiliandev@gmail.com>
2017-10-17 22:39:35 -0200

    1005 - Media ponderada :construction:

--- playcode/uri/1005-average1.py
+++ playcode/uri/1005-average1.py
@@ -1,29 +1,10 @@
-'''
-Read two floating points' values of double precision A and B, corresponding to two student's grades. After this, calculate the student's average, considering that grade A has weight 3.5 and B has weight 7.5. Each grade can be from zero to ten, always with one digit after the decimal point. Don’t forget to print the end of line after the result, otherwise you will receive “Presentation Error”. Don’t forget the space before and after the equal sign.
-
-Input
-
-The input file contains 2 floating points' values with one digit after the decimal point.
-
-Output
-
-Print MEDIA(average in Portuguese) according to the following example, with 5 digits after the decimal point and with a blank space before and after the equal signal.
-
-Input Samples	Output Samples
-5.0
-7.1
-                MEDIA = 6.43182
-0.0
-7.1
-                MEDIA = 4.84091
-10.0
-10.0
-                MEDIA = 10.00000
-'''
 a = float(input())
 b = float(input())
 
-media = (a + b) / 2
+# weight
+wa = 3.5
+wb = 7.5
 
-print("MEDIA = %1.5f" %(media))
-# erro no envio
+mp = (a * wa) + (b * wb) / (wa + wb)
+
+print("MEDIA = %1.5f" %(mp))
