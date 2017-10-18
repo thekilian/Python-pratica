a7b780bed68c7e6bbccb59615d24cd9ea098ab63
Renata Kilian <renatakiliandev@gmail.com>
2017-10-17 22:40:27 -0200

    1008 - salary :white_check_mark:

---
+++ playcode/uri/1008-salary.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+id = int(input())
+hours = int(input())
+per_hour = float(input())
+
+total = hours * per_hour
+
+print("NUMBER = {}".format(id))
+print("SALARY = U$ %1.2f" %(total))
