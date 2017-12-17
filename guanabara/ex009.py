035e30a8e3e3fed81a10cca02df10c1509a65e4f
Renata Kilian <renatakiliandev@gmail.com>
2017-12-17 17:59:51 -0200

    Adicionando exercício 009

---
+++ guanabara/ex009.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+#009 - Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+n = int(input('Digite um número inteiro: '))
+
+print('Olha aí a tabuada do {}'.format(n))
+print('{} vezes 1 é {}'.format(n, n * 1))
+print('{} vezes 2 é {}'.format(n, n * 2))
+print('{} vezes 3 é {}'.format(n, n * 3))
+print('{} vezes 4 é {}'.format(n, n * 4))
+print('{} vezes 5 é {}'.format(n, n * 5))
+print('{} vezes 6 é {}'.format(n, n * 6))
+print('{} vezes 7 é {}'.format(n, n * 7))
+print('{} vezes 8 é {}'.format(n, n * 8))
+print('{} vezes 9 é {}'.format(n, n * 9))
+print('{} vezes 10 é {}'.format(n, n * 10))
