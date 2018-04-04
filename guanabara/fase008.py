a6f6efc8dc392b6f70e5a595d47e1aca489d3dd4
Renata Kilian <renatakiliandev@gmail.com>
2018-04-03 21:46:36 -0300

    Adicionando Fase 8

---
+++ guanabara/fase008.py
@@ -0,0 +1,6 @@
+'''
+#08 - Utilizando Módulos
+- importar bibliotecas (recursos extras que adicionam funcionalidades)
+import bebida = importa todas as bebidas
+from doce import pudim = importa um item da biblioteca apenas
+'''
