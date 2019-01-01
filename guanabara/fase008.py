81ce280b7b82559126c6306b293fa6be0baa92b7
Renata Kilian <renatakiliandev@gmail.com>
2019-01-01 18:44:41 -0200

    Updating fase008 - exercises list added.

--- guanabara/fase008.py
+++ guanabara/fase008.py
@@ -43,10 +43,36 @@ Para pesquisar as bibliotecas: python.org / docs / escolher versão Python - mat
 
 # importar números aleatórios
 import random
-num = random.random()
+num = random.random() // computador dizer n° aleatório entre 0 e 1
 print(num)
 
 import random
-num = random.randint(1, 10)
+num = random.randint(1, 10) // randomizar n° inteiro de 1 a 10
 print(num)
+
+python.org
+PyPI - Python package index (índice de pacotes extras) - lista enorme de pacotes que podemos importar. Tb podemos criar nossas pps bibliotecas, módulos e disponibilizar para a comunidade
+
+import emoji
+print(emoji.emojize("Olá, Mundo :earth_americas: !", use_aliases=True))
+'''
+
+'''
+Desafios:
+***Dica: procurar utilizar as bibliotecas corretas!***
+
+# 016 - Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira. (Ex.:
+    Digite um número: 6.127
+    O número 6.127 tem a parte inteira de 6.)
+
+# 017 - Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
+
+# 018 - Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+
+# 019 - Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+# 020 - O mesmo professor do desafio anterior quer sortear a ordem de apresentação dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+# 021 - Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
 '''
