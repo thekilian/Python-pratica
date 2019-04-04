48a9950f565a9308d36d7e36e81e4e7f9872d424
Renata Kilian <renatakiliandev@gmail.com>
2019-04-03 23:36:23 -0300

    Adding f-string exercise

---
+++ playcode_python/101_f-string.py
@@ -0,0 +1,33 @@
+'''
+Bite 101. f-strings and a simple if/else
+The latest way to print to the screen in Python (>= 3.6) is with f-strings.
+
+In this Bite we'll get you to calculate whether a person is able to drive or not based on their minimum age which is stored in the constant MIN_DRIVING_AGE.
+
+Print name is allowed to drive or name is not allowed to drive (note there is no period), based on the age being equal or greater than MIN_DRIVING_AGE.
+'''
+
+MIN_DRIVING_AGE = 18
+
+def allowed_driving(name, age):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+    if(age >= MIN_DRIVING_AGE):
+        print("{} is allowed to drive".format(name))
+    else:
+        print("{} is not allowed to drive".format(name))
+    pass
+
+allowed_driving("Test", 17)
+
+'''
+# better solution:
+MIN_DRIVING_AGE = 18
+
+def allowed_driving(name, age):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+    is_allowed = 'is allowed' if age >= MIN_DRIVING_AGE else 'is not allowed'
+    print(f'{name} {is_allowed} to drive')
+
+'''
