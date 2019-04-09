73a810411e5b4a96c757746cd3a4215df2cba0ab
Renata Kilian <renatakiliandev@gmail.com>
2019-04-08 23:25:34 -0300

    Adding exercise 033 :construction:

---
+++ guanabara/mundo1/ex033.py
@@ -0,0 +1,11 @@
+'''
+033 - Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+'''
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+
+print('Você digitou os números: {}, {} e {}.'.format(n1, n2, n3))
+# maior =
+# menor =
+# print('O maior número entre eles é {}. \n O menor número entre eles é {}.'.format(maior, menor))
