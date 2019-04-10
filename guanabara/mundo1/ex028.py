ae24a64412fb1f79a1fa03d8910b82af294efbf2
Renata Kilian <renatakiliandev@gmail.com>
2019-04-10 20:51:14 -0300

    Updating ex028 - com firulas

--- guanabara/mundo1/ex028.py
+++ guanabara/mundo1/ex028.py
@@ -3,9 +3,27 @@
 O programa deverá escrever na tela se o usuário venceu ou perdeu.
 '''
 
-import random
-num = random.randint(0, 5)
+'''
+from random import randint
+num = randint(0, 5)
 palpite = int(input('O computador escolheu um número de 0 a 5. \nVocê consegue descobrir qual foi? \nDigite seu palpite: '))
+print('O número escolhido pelo computer foi {}.'.format(num))
+if num == palpite:
+    print('Parabéns, você acertou!')
+else:
+    print("Sorry, not sorry! You're wrong!")
+'''
+
+# com firulas:
+from random import randint
+from time import sleep
+num = randint(0, 5)
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 a 5. Tente adivinhar...')
+print('-=-' * 20)
+palpite = int(input('Digite seu palpite: '))
+print('PROCESSANDO...')
+sleep(2)
 print('O número escolhido pelo computer foi {}.'.format(num))
 if num == palpite:
     print('Parabéns, você acertou!')
