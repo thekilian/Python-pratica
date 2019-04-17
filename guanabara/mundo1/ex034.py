effdd1f33d5567b8e52bbfc7855e379752bea7af
Renata Kilian <renatakiliandev@gmail.com>
2019-04-17 13:02:55 -0300

    Updating ex034 - adding another way to solve it

--- guanabara/mundo1/ex034.py
+++ guanabara/mundo1/ex034.py
@@ -4,13 +4,24 @@ Para salários superiores a R$1.250,00, calcule um aumento de 10%.
 Para os inferiores ou iguais, o aumento é de 15%.
 '''
 
+'''
 salario = float(input('Qual seu salário? R$'))
 print('Seu salário é de R${:.2f}'.format(salario))
 if salario > 1250:
     aumento = (salario * 10) / 100
     total = salario + aumento
-    print('Você receberá R${:.2f} de aumento. \nSeu salário será de R${:.2f}.'.format(aumento, total))
+    print('Você receberá R${:.2f} de aumento.  \nSeu salário será de R${:.2f}.'.format(aumento, total))
 else:
     aumento = (salario * 15) / 100
     total = salario + aumento
     print('Você receberá R${:.2f} de aumento. \nSeu salário será de R${:.2f}.'.format(aumento, total))
+'''
+
+salario = float(input('Qual o salário? R$'))
+print('O salário é R${:.2f}'.format(salario))
+if salario >= 1250:
+    aumento = salario + (salario * 10 / 100)
+else:
+    aumento = salario + (salario * 15 / 100)
+
+print('O salário será de R${:.2f}.'.format(aumento))
