4d8557ddc96e7fdac6031cd8189b5a6571b63229
Renata Kilian <renatakiliandev@gmail.com>
2019-04-17 12:31:37 -0300

    Updating ex032 - adding conditions to verify the years

--- guanabara/mundo1/ex032.py
+++ guanabara/mundo1/ex032.py
@@ -2,20 +2,13 @@
 032 - Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
 '''
 
-ano = int(input('Digite um ano: '))
+from datetime import date
+ano = int(input('Qual ano você quer saber? \nDigite 0 para analisar o ano atual: '))
 print('Você digitou {}'.format(ano))
 
-if (ano % 4 == 0):
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('{} é BISSEXTO!'.format(ano))
 else:
-    print('{} não é bissexto.'.format(ano))
-
-
-'''
-Um ano é bissexto quando ele é divisível por 4. Porém, existe uma exceção. Os anos que terminam por dois zeros serão bissextos se forem divisíveis por 400.
-
-Exemplo: 
-2012 é um ano bissexto. Pois 2012 / 4 = 503, ou seja, uma divisão exata. 
-1998 não é um ano bissexto, pois 1998 / 4 = 499,5, uma divisão inexata.
-5000 não é um ano bissexto pois, apesar de ser divisível por 4, é um número terminado em 00 e não é divisível por 400.
-'''
+    print('{} NÃO é bissexto.'.format(ano))
