00cf41c7d4e64470dad8e73ad7396d0a457f7387
Renata Kilian <renatakiliandev@gmail.com>
2019-04-17 12:53:18 -0300

    Updating ex033 - adding conditions to verify numbers

--- guanabara/mundo1/ex033.py
+++ guanabara/mundo1/ex033.py
@@ -1,11 +1,44 @@
 '''
 033 - Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
 '''
-n1 = int(input('Digite o primeiro número: '))
-n2 = int(input('Digite o segundo número: '))
-n3 = int(input('Digite o terceiro número: '))
+a = int(input('Digite o primeiro número: '))
+b = int(input('Digite o segundo número: '))
+c = int(input('Digite o terceiro número: '))
 
-print('Você digitou os números: {}, {} e {}.'.format(n1, n2, n3))
-# maior =
-# menor =
-# print('O maior número entre eles é {}. \n O menor número entre eles é {}.'.format(maior, menor))
+print('Você digitou os números: {}, {} e {}.'.format(a, b, c))
+
+'''
+if n1 > n2 and n1 > n3:
+    maior = n1
+
+if n2 > n1 and n2 > n3:
+    maior = n2
+
+if n3 > n1 and n3 > n2:
+    maior = n3
+
+if n1 < n2 and n1 < n3:
+    menor = n1
+
+if n2 < n1 and n2 < n3:
+    menor = n2
+
+if n3 < n1 and n3 < n2:
+    menor = n3
+'''
+
+# verificando qual é o maior valor:
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+
+# verificando qual é o menor valor:
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+
+print('O maior número é {}. \nO menor número é {}.'.format(maior, menor))
