a205160b3c7f8a0ed02fe77cfe340ebfd9e848bf
Renata Kilian <renatakiliandev@gmail.com>
2019-05-05 17:19:02 -0300

    Updating ex021

--- guanabara/mundo1/ex021.py
+++ guanabara/mundo1/ex021.py
@@ -1,7 +1,7 @@
 # 021 - Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 import pygame
 pygame.init()
-pygame.mixer.music.load('music.mp3')
+pygame.mixer.music.load('music.wav')
 pygame.mixer.music.play()
 pygame.event.wait()
 
