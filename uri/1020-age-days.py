c8bb94e0ac6627d700395d43e7bcf946308ea5a7
Renata <renatakiliandev@gmail.com>
2019-10-12 13:55:46 -0300

    Exercise 1020 solved

--- uri/1020-age-days.py
+++ uri/1020-age-days.py
@@ -1,13 +1,14 @@
-i = int(input())
-
-a = i / 365
-m = (i - a) / 30
-d = m - a
-
-print("{} ano(s) \n{} mes(es) \n{} dia(s)".format(a, m, d))
-
 '''
 year = 365 days
 month = 30 days
 '''
 
+days = int(input())
+
+a = days // 365
+m = (days - (a * 365)) // 30
+d = days - (a * 365) - (m * 30)
+
+print(f"{a} ano(s)\n{m} mes(es)\n{d} dia(s)")
+
+#print("{} ano(s)\n{} mes(es)\n{} dia(s)".format(a, m, d))
