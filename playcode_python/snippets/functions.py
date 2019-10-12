e347a5695bdfcce3fc2beddcf2f52e5a10858e8a
Renata <renatakiliandev@gmail.com>
2019-10-11 21:18:45 -0300

    Updating Python-pratica

--- playcode_python/snippets/functions.py
+++
@@ -1,6 +0,0 @@
-"""Playlist Funções - Eduardo Mendes"""
-
-def soma_nomeados(x=7, y=7):
-    # X e Y são parâmetros nomeados. Na falta de x ou y, o valor 7 será usado.
-    print(f'x: {x}, y: {y}')
-    return x + y
