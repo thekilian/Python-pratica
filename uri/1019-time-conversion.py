34193cf8a7db56a045e8c4b6a72cfb3cef6071b1
Renata <renatakiliandev@gmail.com>
2019-10-12 13:55:35 -0300

    Exercise 1019 solved

--- uri/1019-time-conversion.py
+++ uri/1019-time-conversion.py
@@ -1,3 +1,4 @@
+'''
 n = int(input())
 
 h = n / 3600
@@ -7,3 +8,11 @@ m = ((h * 3600) - h) / 60
 s = (h * 3600) - (m * 60)
 
 print("{:2.0f}:{:2.0f}:{:2.0f}".format(h, m, s))
+'''
+
+sec = int(input("Digite os segundos: "))
+h = sec // 3600
+m = (sec - (h * 3600)) // 60
+s = (sec - (h * 3600) - (m * 60))
+
+print(f"{h}:{m}:{s}")
