0aa4718047a957354cd52aaea55fda362c2e0640
Renata Kilian <renatakiliandev@gmail.com>
2021-07-14 21:23:04 -0300

    Create 1043_triangulo.py

---
+++ uri/1043_triangulo.py
@@ -0,0 +1,37 @@
+'''
+Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem: Perimetro = XX.X
+
+Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem: Area = XX.X
+
+| Input Sample | Output Samples   |
+| ------------ | ---------------- |
+| 6.0 4.0 2.0  | Area = 10.0      |
+| ------------ | ---------------- |
+| 6.0 4.0 2.1  | Perimetro = 12.1 |
+
+***
+
+a, b, c = input().split()
+
+if triangulo
+    print(perimetro)
+else
+    print(area)
+
+***
+
+Só irá existir um triângulo se, e somente se, os seus lados obedeceram à seguinte regra: um de seus lados deve ser maior que o valor absoluto (módulo) da diferença dos outros dois lados e menor que a soma dos outros dois lados:
+
+| b - c | < a < b + c
+| a - c | < b < a + c
+| a - b | < c < a + b
+
+***
+
+perímetro do triângulo = soma de todos os lados
+
+***
+
+área de um triângulo = (base x altura) / 2
+
+'''
