c22160265a326c03b86730af5645b7a54f11277d
Renata Kilian <renatakiliandev@gmail.com>
2021-07-14 21:23:09 -0300

    Update 1041_coord_ponto.py

--- uri/1041_coord_ponto.py
+++ uri/1041_coord_ponto.py
@@ -18,13 +18,7 @@ x, y = input().split()
 x = float(x)
 y = float(y)
 
-if x == 0.0 and y == 0.0:
-    print("Origem")
-elif x == 0.0 and y > 0.0:
-    print("Eixo X")
-elif x > 0.0 and y == 0.0:
-    print("Eixo Y")
-elif x > 0.0 and y > 0.0:
+if x > 0.0 and y > 0.0:
     print("Q1")
 elif x < 0.0 and y > 0.0:
     print("Q2")
@@ -32,3 +26,9 @@ elif x < 0.0 and y < 0.0:
     print("Q3")
 elif x > 0.0 and y < 0.0:
     print("Q4")
+elif x == 0.0 and y == 0.0:
+    print("Origem")
+elif x == 0.0 and y != 0.0:
+    print("Eixo Y")
+elif x != 0.0 and y == 0.0:
+    print("Eixo X")
