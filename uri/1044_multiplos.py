ccaad6206351ea2dfab69ab2bc550669e5b80e09
Renata Kilian <renatakiliandev@gmail.com>
2021-07-17 18:22:16 -0300

    Create 1044_multiplos.py

---
+++ uri/1044_multiplos.py
@@ -0,0 +1,19 @@
+'''
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+| Input Sample | Output Samples     |
+| ------------ | ------------------ |
+| 6 24         | Sao Multiplos      |
+| ------------ | ------------------ |
+| 6 25         | Nao sao Multiplos  |
+'''
+
+a, b = input().split()
+
+a = int(a)
+b = int(b)
+
+if multiplos:
+    print("Sao Multiplos")
+else:
+    print("Nao sao Multiplos")
