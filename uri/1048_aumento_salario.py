518cf3fbaffca710d412bf6822f9e6e93b391cdb
Renata Kilian <renatakiliandev@gmail.com>
2021-08-02 20:17:47 -0300

    Update 1048_aumento_salario.py

--- uri/1048_aumento_salario.py
+++ uri/1048_aumento_salario.py
@@ -11,12 +11,6 @@ A empresa ABC resolveu conceder um aumento de salários a seus funcionários de 
 
 Leia o salário do funcionário e calcule e mostre o novo salário, bem como o valor de reajuste ganho e o índice reajustado, em percentual.
 
-**Input**
-
-A entrada contém apenas um valor de ponto flutuante, com duas casas decimais.
-
-**Output**
-Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste e o percentual de reajuste ganho, conforme exemplo abaixo.
 
 | Input Sample | Output Samples         |
 | ------------ | ---------------------- |
@@ -32,3 +26,28 @@ Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste e o perce
 |              | Reajuste ganho: 140.00 | 
 |              | Em percentual: 7 %     |
 '''
+# Aplicar um desconto de 20% nada mais é que multiplicar o valor por 0,2 
+
+salario = float(input())
+
+if salario > 0 and salario <= 400.00:
+    reaj = salario * 0.15
+    perc = 15
+elif salario >= 400.01 and salario <= 800.00:
+    reaj = salario * 0.12
+    perc = 12
+elif salario >= 800.01 and salario <= 1200.00:
+    reaj = salario * 0.1
+    perc = 10
+elif salario >= 1200.01 and salario <= 2000.00:
+    reaj = salario * 0.07
+    perc = 7
+elif salario > 2000.00:
+    reaj = salario * 0.04
+    perc = 4
+
+novo = salario + reaj
+
+print("Novo salario: {:.2f}".format(novo))
+print("Reajuste ganho: {:.2f}".format(reaj))
+print("Em percentual: {} %".format(perc))
